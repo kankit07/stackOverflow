75820e8702f5f3ea14d95ff5bab75113d374fa79
k-ankit07 <07.kankit@gmail.com>
2023-08-10 00:48:37 +0530

    add server url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001",
+  baseURL: "https://stackoverflow-2u3w.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
